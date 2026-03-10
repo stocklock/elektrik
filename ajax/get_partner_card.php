82b98f1d63dcda60fc25e7b2c8fd6fb88ec38c4f
stocklock <62253790+stocklock@users.noreply.github.com>
2026-03-10 20:40:39 +0300

    Delete ajax directory

--- ajax/get_partner_card.php
+++
@@ -1,8 +0,0 @@
-<?php
-require '../functions.php';
-$userId = getUserId();
-
-$stmt = $pdo->prepare("SELECT * FROM partner_card WHERE user_id = ?");
-$stmt->execute([$userId]);
-$partner = $stmt->fetch(PDO::FETCH_ASSOC);
-echo json_encode($partner ?? []);
