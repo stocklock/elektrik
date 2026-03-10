6398df79f7f39948485a77c4ea2f90e5619944b0
stocklock <62253790+stocklock@users.noreply.github.com>
2026-03-10 20:41:06 +0300

    Delete moy-nalog-master directory

--- moy-nalog-master/src/Exception/Domain/ForbiddenException.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace Shoman4eg\Nalog\Exception\Domain;
-
-/**
- * @author Tobias Nyholm <tobias.nyholm@gmail.com>
- */
-final class ForbiddenException extends ClientException {}
--- moy-nalog-master/src/Exception/Domain/ValidationException.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace Shoman4eg\Nalog\Exception\Domain;
-
-/**
- * @author Márk Sági-Kazár <mark.sagikazar@gmail.com>
- */
-final class ValidationException extends ClientException {}
